eab4da5a7f1ddf5ca09f8c0c05e1a631765e7f57
Rajat Parbhakar <749084@student.tor.loc>
2019-07-17 16:01:16 -0400

    first commit

---
+++ shoppinglist/main.js
@@ -0,0 +1,12 @@
+const electron = require('electron');
+const url = require('url');
+const path = require('path');
+
+const{app, BrowserWindow} = electron;
+let mainWindow;
+//listen for app to be ready
+newFunction();
+function newFunction() {
+    app.on('ready', function () { });
+}
+
